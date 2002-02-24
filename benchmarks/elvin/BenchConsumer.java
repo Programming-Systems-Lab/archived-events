32b924688227be6062406091486bda64026e2342
rkc10 <rkc10>
2002-02-24 23:34:15 +0000

    Benchmark subscriber for Elvin on a command prompt

---
+++ benchmarks/elvin/BenchConsumer.java
@@ -0,0 +1,53 @@
+import org.elvin.je4.*;
+
+//test receiving speed of elvin
+public class BenchConsumer implements NotificationListener {
+    int got = 0;
+    int limit;
+    long start = 0;
+
+    public BenchConsumer(int num) {
+	limit = num;
+    }
+    
+    public void notificationAction(Notification event) {
+	got++;
+	if (got == 1) {
+	    start = System.currentTimeMillis();
+	}
+	if ((got % limit) == 0) {
+	    long result = System.currentTimeMillis() - start;
+	    System.out.println("Elapsed time: " + result + " Receive speed: " + ((limit * 1000.0)/result));
+	    
+	    //System.exit(0);
+	}
+	    
+	
+    }
+
+    public static void main(String args[]) throws Exception {
+	if (args.length != 2) {
+	    System.out.println("Usage: BenchConsumer [elvin server] [number of events]");
+	    return;
+	}
+
+	ElvinURL serverName = new ElvinURL(args[0]);
+
+	Consumer cons = new Consumer(serverName);
+
+	int numOfEvents = Integer.parseInt(args[1]);
+
+	Subscription sub = new Subscription("require(dummy)");
+
+	sub.addNotificationListener(new BenchConsumer(numOfEvents));
+
+	cons.addSubscription(sub);
+
+	System.out.println("waiting for events...");
+    }
+}
+
+
+
+
+
